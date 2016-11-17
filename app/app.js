c4f2bb71b004a13b2d4a8c9c7a144847cbbba8ed
Luis Larghi <windinglair@gmail.com>
2016-11-17 20:51:00 -0300

    Checking first if username exist

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 angular.module('app', [])
     .controller('gitHubController', ['$scope', '$http', function ($scope, $http) {
         
-        if ($scope.username.length > 4) {
-            $http.get("https://api.github.com/users/" + $scope.username)
-                .success(function (data) {
-                    console.log(data);
-                })
+        if ($scope.username) {
+            if ($scope.username.length > 4) {
+                $http.get("https://api.github.com/users/" + $scope.username)
+                    .success(function (data) {
+                        console.log(data);
+                    })
+            }
         }
 }]);
