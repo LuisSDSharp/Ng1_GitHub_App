ce28cfc8ad8d05affac9d7ea6f5de0cc9a384035
Xertz <windinglair@gmail.com>
2016-11-17 21:55:56 -0300

    Added table for showing GH users and followers

--- app/app.js
+++ app/app.js
@@ -1,13 +1,19 @@
 angular.module('app', [])
     .controller('gitHubController', ['$scope', '$http', function ($scope, $http) {
         var minInputLength = 4;
+        $scope.userFound = false;
+        $scope.usersData = {};
 
         $scope.onUserInputChanged = function() {
             if ($scope.username.length > minInputLength) {
-                console.log("FUNCA");
                 $http.get("https://api.github.com/users/" + $scope.username)
                     .success(function (data) {
+                        $scope.userFound = true;
+                        $scope.usersData = data;
                         console.log(data);
+                    })
+                    .error(function() {
+                        $scope.userFound = false;
                     })
             }
         };
