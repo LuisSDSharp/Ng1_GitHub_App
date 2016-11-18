871aeb7ab3ab2d0f3ec495e08a305f2472440a13
Xertz <windinglair@gmail.com>
2016-11-17 22:42:34 -0300

    Debugging

--- app/app.js
+++ app/app.js
@@ -1,20 +1,26 @@
 angular.module('app', [])
     .controller('gitHubController', ['$scope', '$http', function ($scope, $http) {
-        var minInputLength = 4;
+        var minInputLength = 3;
         $scope.userFound = false;
-        $scope.usersData = {};
+        $scope.usersData = [];
 
         $scope.onUserInputChanged = function() {
             if ($scope.username.length > minInputLength) {
                 $http.get("https://api.github.com/users/" + $scope.username)
                     .success(function (data) {
                         $scope.userFound = true;
-                        $scope.usersData = data;
+                        
+                        // Coverting object to array
+                        for (var key in data) {
+                            var tempObj = {}
+                            tempObj[key] = data[key];
+                            $scope.usersData.push(tempObj);
+                        }
+                        
                         console.log(data);
                     })
-                    .error(function() {
-                        $scope.userFound = false;
-                    })
+            } else {
+                $scope.userFound = false;
             }
         };
 }]);
