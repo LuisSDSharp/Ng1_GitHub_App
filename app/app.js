c13cb8d06a25261bd240d8e3b239240e54ab3e0a
Xertz <windinglair@gmail.com>
2016-11-17 23:14:13 -0300

    Fixed user display and followers

--- app/app.js
+++ app/app.js
@@ -9,10 +9,15 @@ angular.module('app', [])
                     .then(function (data) {
                         $scope.userFound = true;
                         
-                        $scope.usersData = data.data;
-
+                        $scope.usersData = data;
                         console.log($scope.usersData);
-                    })
+
+                        $http.get("https://api.github.com/users/" + $scope.username + "/following{/other_user}")
+                            .then(function(data) {
+                                $scope.userFollowersData = data;
+                                console.log($scope.userFollowersData);
+                            });
+                    });
             } else {
                 $scope.userFound = false;
             }
